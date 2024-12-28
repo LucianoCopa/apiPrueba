e2c697d99b09c4bf19f9bcaee382821b028f893c
Luciano Copa <45108640@ifts24.edu.ar>
2024-12-28 17:41:27 -0300

    Configuraciones

--- src/main/java/com/spring/datajpa/DataJpaApplication.java
+++ src/main/java/com/spring/datajpa/DataJpaApplication.java
@@ -14,4 +14,17 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
